3f7c92344c8201539949da9d099a539e9b1866a0
oscarbaltasar <oscarbaltasar@hotmail.com>
2024-12-14 14:06:23 +0100

    Updated example

--- script.lua
+++ script.lua
@@ -1,11 +1,18 @@
--- Auto generated script file --
+-- Module initialisation, it MUST be created this way --
 
 local RainyTexture = require("RainyTexture")
 local myRainyTexture = RainyTexture.CreateTexture(textures:getTextures()[1],1, vec(0, 0, 1, 1), 0.1, 10, 20, 1)
+
+-- Example with individual column enable and disable
+--createTexture(textures:getTextures()[1],1, vec(0, 0, 1, 1), 0.1, 10, 20, 1, {true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false})
+
+
+-- Update trail after initialisation
 myRainyTexture.updateTrailTail(10)
 
---createTexture(textures:getTextures()[10],10, vec(0, 0, 0, 1), 0.1, 10, 20, 1, {true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false})
+
 
 function events.tick()
+	-- Update color each tick
 	myRainyTexture.updateSpawnColor(vec(math.random(), math.random(), math.random(), 1))
 end
